5f32ca54fc298f590d515cd861223c1b868a6124
Ravi Kumar <ravikumar10593@gmail.com>
2020-04-08 08:16:37 +0530

    added index.js file

--- commands.php
+++ commands.php
@@ -3,10 +3,10 @@ git config --global user.name "Ravi Kumar"
 
 Learn git Part -1 :
 
-1 git init
-2 git add [filename] || -A
-3 git status
-4 git commit -m "msg"
+1 git init 		// git project bana deti h
+2 git add [filename] || -A 			//add file
+3 git status  
+4 git commit -m "msg" //snapshot
 5 git remote add origin repoName
 6 git push origin master
 7 git clone repoName
